63826b9bdb5deff6027704d5c451112215ea01ac
Jason Haslam <jason@scitools.com>
2016-08-01 14:09:50 -0600

    fetch: work around failure to fetch annotated tags added to existing commits

--- src/libgit2/fetch.c
+++ src/libgit2/fetch.c
@@ -31,7 +31,7 @@ static int maybe_want(git_remote *remote, git_remote_head *head, git_refspec *ta
 	if (!valid)
 		return 0;
 
-	if (tagopt == GIT_REMOTE_DOWNLOAD_TAGS_ALL) {
+	if (tagopt != GIT_REMOTE_DOWNLOAD_TAGS_NONE) {
 		/*
 		 * If tagopt is --tags, always request tags
 		 * in addition to the remote's refspecs
